d22a45d3d37b2b8289536b795c473a909267a59e
Mike Cohen <mjoecohen@gmail.com>
2022-09-15 16:48:24 +0100

    removed pre-rendering options for node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,21 @@
 import adapter from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+// import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [
-    preprocess({
-      scss: {
-        prependData: '@use "src/variables.scss" as *;',
-      },
-    }),
-  ],
+  //preprocess: [
+  //  preprocess({
+  //    scss: {
+  //      prependData: '@use "src/variables.scss" as *;',
+  //    },
+  //  }),
+  //],
   kit: {
     adapter: adapter(),
-    //adapter: adapter({
-    //  fallback: '200.html'
-    //})
-    prerender: { default: true }
+    //prerender: { default: true }
   }
 };
 
